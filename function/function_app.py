59b5750ff0b992e4db37ebabe0ba78cbe05002d9
Corey Campbell <ccampbell@rosemedical.com>
2025-08-19 16:37:37 -0400

    functioning code locally

--- function/function_app.py
+++ function/function_app.py
@@ -1,13 +1,30 @@
 import azure.functions as func
 import logging
+import json
 
 app = func.FunctionApp()
 
 @app.event_hub_message_trigger(arg_name="azeventhub", event_hub_name="events",
                                connection="TempReader_RootManageSharedAccessKey_EVENTHUB") 
-def CosmosDbWriter(azeventhub: func.EventHubEvent):
+
+@app.cosmos_db_output(arg_name="outputDocument", database_name="WeatherReadingDb", 
+                      container_name="TemperatureData", connection="CosmosDbConnection") 
+
+def CosmosDbWriter(azeventhub: func.EventHubEvent, outputDocument: func.Out[func.Document]):
     logging.info('Python EventHub trigger processed an event: %s',
                 azeventhub.get_body().decode('utf-8'))
+    
+    #Get the incoming message as a JSON object
+    Message_body = azeventhub.get_body().decode('utf-8')
+    data = json.loads(Message_body)
+
+    #Add a timestamp and device ID to the document
+    data['timestamp'] = azeventhub.enqueued_time.isoformat()
+    data['deviceId'] = azeventhub.get_system_properties()['iothub-connection-deviceid']
+
+    outputDocument.set(func.Document.from_dict(data))
+
+
 
 
 # This example uses SDK types to directly access the underlying EventData object provided by the Event Hubs trigger.
